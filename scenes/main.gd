a26e1da27ae9275b3d83b2f0e6aea038502991dd
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-01 15:03:04 +0000

    Organized main.gd with groups

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,25 +1,27 @@
 extends Node2D
 
 @onready var root: Window = get_tree().get_root()
-@export var camera_2d: Camera2D
 
-# colours
+@export_group("Nodes")
+@export var camera_2d: Camera2D
+@export var edge_colliders: Array[CollisionShape2D]
+
+@export_group("Game Settings")
+@export var collums: int = 4
+@export var rows: int = 4
+
+@export_group("Colours")
 @export var colours: Array[Color]
 @export var line_colour: Color = Color("ffffff7f")
 @export var background_colour: Color = Color("1e1e1e")
 
-# game
-@export var collums: int = 4
-@export var rows: int = 4
-@export var edge_colliders: Array[CollisionShape2D]
-
-# ball
-@export var ball_scene: PackedScene = preload("res://scenes/ball.tscn")
+@export_group("Ball Settings")
+const ball_scene: PackedScene = preload("res://scenes/ball.tscn")
 @export var ball_start_force: float = 1024
 @export var ball_scale: float = 1.0
 
-# block
-@export var block_scene: PackedScene = preload("res://scenes/block.tscn")
+@export_group("Block Settings")
+const block_scene: PackedScene = preload("res://scenes/block.tscn")
 @export var block_scale: float = 1.0
 @export var scale_position: bool = false
 const BLOCK_SIZE: int = 128
@@ -33,7 +35,7 @@ func _ready() -> void:
 	# set camera pos
 	var scale_pos = 1.0 if !scale_position else block_scale
 	var center_x = collums * BLOCK_SIZE * scale_pos
-	var center_y = rows * BLOCK_SIZE * .5 * scale_pos
+	var center_y = rows * BLOCK_SIZE * 0.5 * scale_pos
 	camera_2d.position = Vector2(center_x, center_y)
 	
 	# init block positions
@@ -56,22 +58,22 @@ func _ready() -> void:
 				block_parents[player].add_child.call_deferred(block)
 	
 	# set players
-	const x_padding = 128
+	const x_padding = 128.0
 	spawn_ball(0, Vector2(x_padding, center_y))
-	spawn_ball(1, Vector2(center_x * 2 - x_padding, center_y))
+	spawn_ball(1, Vector2(center_x * 2.0 - x_padding, center_y))
 	
 	# set edge colliders
-	var top_right = Vector2(center_x * 2, 0)
+	var top_right = Vector2(center_x * 2.0, 0.0)
 	var s_top = edge_colliders[0].shape as SegmentShape2D
 	s_top.a = Vector2.ZERO
 	s_top.b = top_right
 	
-	var bot_right = Vector2(center_x * 2, center_y * 2)
+	var bot_right = Vector2(center_x * 2.0, center_y * 2.0)
 	var s_right = edge_colliders[1].shape as SegmentShape2D
 	s_right.a = top_right
 	s_right.b = bot_right
 	
-	var bot_left = Vector2(0, center_y * 2)
+	var bot_left = Vector2(0, center_y * 2.0)
 	var s_bot = edge_colliders[2].shape as SegmentShape2D
 	s_bot.a = bot_right
 	s_bot.b = bot_left
