55d258c129727c6a86695f2ce6d897daa7ef6ed6
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-09 16:24:52 +0000

    custom drawing balls

--- scenes/main.gd
+++ scenes/main.gd
@@ -27,6 +27,7 @@ const BALL_SCENE: PackedScene = preload("res://scenes/ball.tscn")
 @export var ball_start_force: float = 2048
 @export var ball_scale: float = .5
 var parents_ball: Array[Node]
+var balls: Array[Array] = [[], []]
 
 @export var show_hit_particle: bool = true
 const BALL_HIT_PARTICLE = preload("res://scenes/ball_hit_particle.tscn")
@@ -41,7 +42,7 @@ var trail_gradients: Array[Gradient]
 const BLOCK_SCENE: PackedScene = preload("res://scenes/block.tscn")
 var all_blocks: Array[Array] = [[], []]
 @export var block_scale: float = 1.0
-@export var scale_position: bool = false
+@export var scale_position: bool = true
 const BLOCK_SIZE: int = 128
 const HALF_BLOCK_SIZE: int = 64
 
@@ -86,7 +87,6 @@ func _ready() -> void:
 		g.set_color(1, colours[i])
 		trail_gradients.append(g)
 		
-		main_ui.set_score(i, collums * rows)
 		
 		var parent_block = Node.new()
 		parent_block.set_name("block_parent_%d" % i)
@@ -108,9 +108,11 @@ func _ready() -> void:
 				all_blocks[i].append(block)
 				
 				parent_block.add_child.call_deferred(block)
+		
+		main_ui.set_score(i, all_blocks[i].size())
 	
 	# set balls
-	var ball_padding = BLOCK_SIZE * ball_scale * 2.0
+	var ball_padding = BLOCK_SIZE * scale_pos
 	var nearest_sqrt = pow(ceil(sqrt(ball_amount)), 2.0)
 	# TODO: in the future it would be nice to have a grid that is not just squared,
 	# One that respects the size of the ball area (collums & rows)
@@ -159,9 +161,19 @@ func _ready() -> void:
 		segment.b = points[(i + 1) % 4]
 
 
+func _draw() -> void:
+	var radius = BLOCK_SIZE * ball_scale * .5
+	for i in 2:
+		var colour = colours[i]
+		var size = balls[i].size()
+		for j in size:
+			draw_circle(balls[i][j].position, radius, colour)
+
+
 func _process(delta: float) -> void:
 	elapsed_time += delta
 	main_ui.set_time(elapsed_time)
+	queue_redraw()
 
 
 func _input(event: InputEvent) -> void:
@@ -175,25 +187,26 @@ func _input(event: InputEvent) -> void:
 func spawn_ball(num: int, index: int, pos: Vector2) -> void:
 	var ball: Ball = BALL_SCENE.instantiate()
 	ball.set_name("ball_%d" % num)
-	ball.set_id(index, colours[index])
+	ball.set_id(index)
 	ball.trail.set_gradient(trail_gradients[index])
 	ball.trail.set_state(show_ball_trail)
-	ball.set_pos_scale(pos, ball_scale)
+	ball.set_pos_scale(pos, ball_scale - (ball_scale * 0.05))
 	ball.on_hit.connect(on_ball_hit)
 	var theta = randf() * TAU
 	var random_inside_unit_circle = Vector2(cos(theta), sin(theta)) * sqrt(randf())
 	ball.launch(random_inside_unit_circle.normalized() * ball_start_force)
+	balls[index].append(ball)
 	parents_ball[index].add_child.call_deferred(ball)
 
 
-func on_ball_hit(hit_point: Vector2, normal: Vector2, colour: Color) -> void:
+func on_ball_hit(hit_point: Vector2, normal: Vector2, id: int) -> void:
 	if !show_hit_particle:
 		return
 	
 	if ball_hit_particles.size() == 0:
 		spawn_ball_hit_particle()
 	
-	ball_hit_particles.pop_front().play(hit_point, normal, colour)
+	ball_hit_particles.pop_front().play(hit_point, normal, colours[id])
 
 
 func spawn_ball_hit_particle() -> void:
