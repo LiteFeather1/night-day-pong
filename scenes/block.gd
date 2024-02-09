5b2a9eb6b44605a666cb3dd995d1d060ef5664fa
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-02-09 21:02:17 +0000

    Removed legacy code and nodes from Block

--- scenes/block.gd
+++ scenes/block.gd
@@ -4,20 +4,13 @@ extends StaticBody2D
 
 signal hit(block: Block)
 
-@export var sprite_line: Sprite2D
-@export var sprite_block: Sprite2D
 
 var id: int
-
-
-func set_line(colour: Color) -> void:
-	sprite_line.modulate = colour
 
 
 func set_layer(layer_id: int, colour: Color) -> void:
 	id = layer_id
 	set_collision_layer_value(layer_id + 3, true)
-	sprite_block.modulate = colour
 
 
 func flip() -> void:
